0e4a5533e7ab674bc62cc9e00f7a66774166ad94
Lukash <lukashn159@gmail.com>
2025-12-15 20:58:20 +0545

    Practicing Folium

--- Folium_practice.py
+++ Folium_practice.py
@@ -2,3 +2,14 @@ import folium
 
 map = folium.Map()
 map.save("Map.html")
+
+latitude_of_everest = 27.9881
+longitude_of_everest = 86.9250
+
+folium.CircleMarker(
+        location=[latitude_of_everest, longitude_of_everest],
+        radius=5,
+        color="red",
+        fill=True
+    ).add_to(map)
+map.save("Map.html")
